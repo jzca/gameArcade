afedf47a92121314b0e23f8987ef75a694829020
Jerry <contactmevia@githuborothers.com>
2018-12-10 09:57:10 -0600

    add imgmin

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,25 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
-gulp.task('cssfix',()=> {
+gulp.task('cssfix', () => {
 	return gulp.src('src/css/*.css')
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
-	}))
+		}))
 		.pipe(gulp.dest('dist/css'));
 })
 
-gulp.task('cssmin',()=> {
+gulp.task('cssmin', () => {
 	return gulp.src('dist/css/*.css')
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('dist/css'));
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('dist/css'));
 })
 
-
-
-
-
+gulp.task('imgmin', () => {
+	gulp.src('src/images/*')
+		.pipe(imagemin({
+			verbose: true
+		}))
+		.pipe(gulp.dest('dist/images'))
+});
